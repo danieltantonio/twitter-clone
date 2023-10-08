0602c4ea335f072d1bd933171458a9b5b5fee9ef
Daniel <danieltomantonio@gmail.com>
2023-10-08 17:55:00 +0100

    Add loading spinner after login

--- components/Modals/LoginModal.tsx
+++ components/Modals/LoginModal.tsx
@@ -6,6 +6,7 @@ import Button from "@/components/Inputs/Buttons/Button";
 import { useState, FormEvent } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/navigation";
+import { Spinner } from "@material-tailwind/react";
 
 type LoginForm = {
     email: string,
@@ -19,6 +20,7 @@ const initLoginForm: LoginForm = {
 
 export default function LoginModal() {
     const [loginForm, setLoginForm] = useState(initLoginForm);
+    const [isLoading, setIsLoading] = useState(false);
     const supabase = createClientComponentClient();
     const router = useRouter();
 
@@ -28,16 +30,21 @@ export default function LoginModal() {
     }
 
     async function handleLogin() {
+        setIsLoading(true);
         const { email, password } = loginForm;
         const { data, error } = await supabase.auth.signInWithPassword({ email, password });
-        
-        if(data.user) {
+
+
+        if (data.user) {
             router.push("/home");
+            setIsLoading(false);
+        } else {
+            setIsLoading(false);
         }
     }
 
     return (
-        <div className="w-3/5 mx-auto px-8">
+        <div className="w-3/5 mx-auto px-8 relative">
             <div className="my-8">
                 <span className="font-bold text-3xl">Sign in to Twitter</span>
             </div>
@@ -65,6 +72,17 @@ export default function LoginModal() {
             <div className="my-10 pb-20 text-sm">
                 <span className="text-slate/75">Don't have an account? <span className="text-primary cursor-pointer">Sign up</span></span>
             </div>
+            {
+                isLoading && (
+                    <div className="absolute h-full w-full top-0 left-0 bg-black">
+                        <div className="flex flex-row w-full h-full justify-center">
+                            <div className="flex flex-col h-full justify-center">
+                                <Spinner color="blue" className="h-12 w-12" />
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     )
 }
